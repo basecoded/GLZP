d54edcb8c55556cf0fe26dbf676915ea01449a7d
hsc <827773271@qq.com>
2016-05-25 11:14:49 +0800

    push by hsc

---
+++ Classes/WelcomeLayer.cpp
@@ -0,0 +1,50 @@
+﻿#include "WelcomeLayer.h"
+#include "HelloWorldScene.h"
+#include "ui/UIButton.h"
+using namespace ui;
+WelcomeLayer::WelcomeLayer()
+{
+}
+
+WelcomeLayer::~WelcomeLayer()
+{
+}
+
+bool WelcomeLayer::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+	addUI(); 
+	return true;
+}
+Scene* WelcomeLayer::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = WelcomeLayer::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+void WelcomeLayer::addUI()
+{
+	auto btn = Button::create("CloseNormal.png");
+	btn->addClickEventListener(CC_CALLBACK_1(WelcomeLayer::btnCallback,this));
+	btn->setScale(5.0f);
+	addChild(btn);
+	btn->setPosition(Vec2(SIZE.width / 2, SIZE.height / 2));
+}
+
+void WelcomeLayer::btnCallback(Ref* sender)
+{
+	auto s = HelloWorld::createScene();
+	Director::getInstance()->replaceScene(s);
+}
---
+++ Classes/WelcomeLayer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "cocos2d.h"
+USING_NS_CC;
+#define SIZE Director::getInstance()->getVisibleSize()
+
+class WelcomeLayer: public Layer
+{
+
+public:
+
+	WelcomeLayer();
+	~WelcomeLayer();
+	virtual bool init();
+	static Scene* createScene();
+	CREATE_FUNC(WelcomeLayer);
+
+	void addUI();
+	void btnCallback(Ref* sender);
+private:
+
+};
+
